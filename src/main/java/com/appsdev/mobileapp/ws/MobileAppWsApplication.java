2ccdbd27517592053b53415df89eaae34b3a0267
jericho <echocastro18@gmail.com>
2020-12-28 17:26:06 +0800

    Set war file to deploy in standalone tomcat

--- src/main/java/com/appsdev/mobileapp/ws/MobileAppWsApplication.java
+++ src/main/java/com/appsdev/mobileapp/ws/MobileAppWsApplication.java
@@ -1,15 +1,21 @@
 package com.appsdev.mobileapp.ws;
 
-import com.appsdev.mobileapp.ws.security.AppProperties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import javax.swing.*;
 
 @SpringBootApplication
-public class MobileAppWsApplication {
+public class MobileAppWsApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MobileAppWsApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MobileAppWsApplication.class, args);
@@ -23,6 +29,7 @@ public class MobileAppWsApplication {
     @Bean
     public SpringApplicationContext springApplicationContext() {
         return new SpringApplicationContext();
+
     }
 
 }
